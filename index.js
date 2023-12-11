4252543538f7e74498ccc8851be39d2015d15aca
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-12-11 23:00:52 +0600

    MailGun Used to send messages by clients

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const jsonwebtoken = require('jsonwebtoken');
 
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+    username: 'api',
+    key: process.env.MAILGUN_API_KEY,
+});
 /* database name */
-const DB = "swiftWheels-db";
+const DB = "ibn-aroub";
 
 const app = express();
 
@@ -14,9 +21,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        "https://mahogany-furniture-mnh.web.app",
-        "https://mahogany-furniture-mnh.firebaseapp.com"
+        'http://localhost:6173',
     ],
     credentials: true
 }));
@@ -68,8 +73,7 @@ const setTokenCookie = async (req, res, next) => {
     }
 }
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@carsdoctordb.pehv7ki.mongodb.net/${DB}?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@projects.mqfabmq.mongodb.net/${DB}?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -86,6 +90,7 @@ async function run() {
 
         const IA_database = client.db(DB);
         const projectCollection = IA_database.collection('projects');
+        const emailCollection = IA_database.collection('emails');
 
         /* Auth api */
         app.post('/api/v1/auth/jwt', setTokenCookie, async (req, res) => {
@@ -125,7 +130,7 @@ async function run() {
             try {
                 const result = await projectCollection.find().toArray();
 
-                console.log(result);
+                // console.log(result);
 
                 res.send(result)
             } catch (error) {
@@ -133,429 +138,29 @@ async function run() {
             }
         })
 
-        // /* find Popular services */
-        // app.get('/api/v1/popular-services', async (req, res) => {
-        //     try {
-        //         const { type } = req.query;
+        /* Send Email */
+        app.post('/api/v1/send-message', async (req, res) => {
+
+            const userMessage = req.body;
+            // console.log(userMessage);
+
+            /* Save to database */
+            const result = await emailCollection.insertOne(userMessage);
+
+            // console.log(result);
+
+            mg.messages
+                .create(process.env.MAILGUN_SENDING_DOMAIN, {
+                    from: userMessage?.email,
+                    to: [ "mdneamul99@student.sust.edu" ],
+                    subject: userMessage?.title,
+                    text: userMessage?.name,
+                    html: userMessage?.message
+                })
+                .then(msg => res.send(msg))
+                .catch(err => console.log(err));
+        })
 
-        //         const options = {
-        //             projection: { title: 1, price: 1, img: 1, provider: 1, description: 1 }
-        //         }
-
-        //         if (type === "bikes") {
-        //             const result = await bikeCollection.find({}, options).limit(4).toArray();
-        //             return res.send(result)
-        //         }
-        //         const result = await carCollection.find({}, options).limit(4).toArray();
-
-        //         // console.log(result);
-
-        //         /* if not matched send empty array */
-        //         return res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Get services provided by the user */
-        // app.get('/api/v1/user/services/:email', verifyToken, async (req, res) => {
-        //     try {
-        //         const email = req?.params?.email;
-
-        //         // console.log(email, req.user?.email, email === req?.user?.email);
-
-        //         if (email !== req?.user?.email) return res.status(403).send({ message: 'Forbidden' })
-
-        //         const query = { "provider.email": email }
-
-        //         const bikes = await bikeCollection.find(query).toArray();
-
-        //         const cars = await carCollection.find(query).toArray();
-
-        //         const result = [ ...bikes, ...cars ];
-
-        //         /* if not matched send empty array */
-        //         return res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Get services of this provider */
-        // app.get('/api/v1/same-provider-services/:email', async (req, res) => {
-        //     try {
-        //         const email = req.params.email;
-        //         const { id } = req.query;
-
-        //         // console.log(email);
-
-        //         const query = { "provider.email": email, _id: { $ne: new ObjectId(id) } }
-
-        //         const options = {
-        //             projection: {
-        //                 img: 1,
-        //                 type: 1,
-        //                 name: 1,
-        //                 area: 1,
-        //                 description: 1,
-        //                 price: 1,
-        //                 status: 1,
-        //             }
-        //         }
-        //         // console.log(email, query);
-
-        //         const bikes = await bikeCollection.find(query, options).toArray();
-
-        //         const cars = await carCollection.find(query, options).toArray();
-
-        //         const result = [ ...bikes, ...cars ];
-        //         // console.log([ ...bikes, ...cars ]);
-
-        //         /* if not matched send empty array */
-        //         return res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Get a service by ID */
-        // app.get('/api/v1/services/:id', async (req, res) => {
-        //     try {
-        //         const { id } = req.params;
-
-        //         console.log("Service id: ", id);
-
-        //         const query = { _id: new ObjectId(id) }
-
-        //         // if (type === "bikes") {
-
-        //         const result1 = await bikeCollection.find(query).toArray();
-        //         if (!result1.length) {
-        //             const result = await carCollection.find(query).toArray();
-
-        //             console.log('the service: (244)', result);
-
-        //             /* if not matched send empty array */
-        //             return res.send(result)
-        //         }
-
-        //         return res.send(result1)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Get a service by Title */
-        // app.get('/api/v1/filtered-services/:title', async (req, res) => {
-        //     try {
-        //         const { title } = req.params;
-        //         const { type } = req.query;
-
-        //         // console.log("Service title: ", title);
-
-        //         const query = { title: { $regex: title, $options: 'i' } }
-
-        //         if (type === "bikes") {
-        //             const result = await bikeCollection.find(query).toArray();
-
-        //             // console.log(result);
-
-        //             /* if not matched send empty array */
-        //             return res.send(result)
-        //         }
-        //         const result = await carCollection.find(query).toArray();
-
-        //         // console.log(result);
-        //         return res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* get by ids */
-        // app.post('/api/v1/services', async (req, res) => {
-
-        //     try {
-        //         const { ids } = req.body;
-
-        //         console.log("Service ids: ", ids);
-
-        //         const objectIds = ids.map(id => new ObjectId(id))
-
-        //         const query = { _id: { $in: objectIds } }
-
-        //         // if (type === "bikes") {
-        //         const options = {
-        //             projection: { _id: 1 }
-        //         }
-
-        //         const bikes = await bikeCollection.find(query, options).toArray();
-        //         // console.log(result);
-        //         // }
-        //         const cars = await carCollection.find(query, options).toArray();
-
-        //         const result = [ ...bikes, ...cars ];
-        //         console.log([ ...bikes, ...cars ]);
-
-        //         /* if not matched send empty array */
-        //         return res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Add / Host a service */
-        // app.post('/api/v1/create-service', verifyToken, async (req, res) => {
-
-        //     try {
-        //         const service = req.body;
-
-        //         if (service.provider?.email !== req.user?.email) return res.status(403).send({ message: 'Forbidden' })
-
-        //         console.log(service);
-        //         if (service?.type === 'bikes') {
-        //             const result = await bikeCollection.insertOne(service)
-
-        //             // console.log(result);
-        //             return res.send(result)
-        //         }
-        //         const result = await carCollection.insertOne(service)
-
-        //         // console.log(result);
-        //         res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Update service by ID */
-        // app.patch('/api/v1/update-service/:id', async (req, res) => {
-
-        //     try {
-        //         const { id } = req.params;
-        //         const { type } = req.query;
-        //         const updatedService = req.body;
-
-        //         console.log("Update services: ", id);
-
-        //         const query = { _id: new ObjectId(id) }
-
-        //         if (type === 'bikes') {
-        //             const result = await bikeCollection.updateOne(
-        //                 query,
-        //                 { $set: { ...updatedService } },
-        //             );
-
-        //             console.log(result);
-        //             return res.send(result)
-        //         }
-
-        //         const result = await carCollection.updateOne(
-        //             query,
-        //             { $set: { ...updatedService } },
-        //         );
-        //         console.log(result);
-
-        //         return res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* delete provided service */
-        // app.delete('/api/v1/user/delete-service/:id', verifyToken, async (req, res) => {
-
-        //     try {
-        //         const { id } = req.params;
-        //         const { email } = req.query;
-
-        //         // console.log("Delete service: ", id, email);
-
-        //         if (email !== req?.user?.email) return res.status(403).send({ message: 'Forbidden' })
-
-        //         const query = { _id: new ObjectId(id), "provider.email": email };
-
-        //         const result1 = await bikeCollection.deleteOne(query)
-        //         if (result1?.data?.deletedCount)
-        //             return res.send(result1)
-
-        //         const result = await carCollection.deleteOne(query)
-
-        //         // console.log(result);
-        //         res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Get all bookings */
-        // app.get('/api/v1/bookings/:email', verifyToken, async (req, res) => {
-        //     try {
-        //         const email = req.params.email;
-
-        //         console.log("Bookings:", email, req.user.email);
-
-        //         if (email !== req?.user?.email) return res.status(403).send({ message: 'Forbidden' })
-
-        //         const result = await bookingCollection.find({ "Owner.email": email }).toArray();
-
-        //         // console.log('Bookings: ', result);
-
-        //         res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Get a booking by id */
-        // app.get('/api/v1/bookings/:id', async (req, res) => {
-        //     try {
-        //         const id = req.params.id;
-        //         const query = { _id: new ObjectId(id) }
-
-        //         const options = {
-        //             projection: {
-        //                 count: 1
-        //             }
-        //         }
-        //         console.log("The booking: ", id, query, options);
-
-        //         const result = await bookingCollection.findOne(query, options);
-
-        //         // console.log(result);
-
-        //         res.send(result)
-        //     } catch (error) {
-        //         console.log(error);
-        //         res.status(500).send({ message: 'Internal server error' });
-        //     }
-        // })
-
-        // /* delete a booking */
-        // app.delete('/api/v1/user/cancel-booking/:id', verifyToken, async (req, res) => {
-
-        //     try {
-        //         const { id } = req.params;
-        //         const { email } = req.query;
-
-        //         // console.log(id, email);
-
-        //         if (email !== req.user?.email) return res.status(403).send({ message: 'Forbidden' })
-
-        //         const query = { _id: new ObjectId(id), "Owner.email": email };
-        //         const result = await bookingCollection.deleteOne(query)
-
-        //         /* Update Provider schedule */
-        //         if (result.deletedCount) {
-        //             const query1 = { _id: new ObjectId(id) }
-        //             const serviceStatus = {
-        //                 $set: {
-        //                     statusInfo: {
-        //                         status: 'available',
-        //                         income: null,
-        //                         schedule: null
-        //                     }
-        //                 }
-        //             }
-
-        //             const result1 = await bikeCollection.updateOne(
-        //                 query1,
-        //                 serviceStatus,
-        //             );
-
-        //             if (!result1.modifiedCount) {
-        //                 await carCollection.updateOne(
-        //                     query1,
-        //                     serviceStatus,
-        //                 );
-        //             }
-
-        //             if (result1.modifiedCount) result.driverInformed = true;
-        //         }
-
-        //         // console.log(result);
-        //         res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* booked a service [customize for desired solution] */
-        // app.patch('/api/v1/update-booking/:id', async (req, res) => {
-        //     try {
-        //         const { id } = req.params;
-        //         const booking = req.body;
-        //         // booking._id = new ObjectId(id);
-
-        //         const query = { _id: new ObjectId(id) }
-
-        //         const Update = {
-        //             $set: {
-        //                 ...booking
-        //             }
-        //         }
-
-        //         console.log(
-        //             "Update booking: ", Update
-        //         );
-
-        //         const result = await bookingCollection.updateOne(query, Update, { upsert: true })
-
-
-        //         // console.log(result);
-
-        //         return res.send(result)
-        //     } catch (error) {
-        //         console.log(error);
-        //         res.status(500).send({ message: 'Internal server error' });
-        //     }
-        // })
-
-        // app.post('/api/v1/book-service', async (req, res) => {
-        //     try {
-        //         const booking = req?.body;
-        //         const { id } = req?.query;
-
-        //         booking._id = new ObjectId(id)
-
-        //         console.log("Book service: ", booking);
-
-        //         const result = await bookingCollection.insertOne(booking)
-
-        //         return res.send(result)
-        //     } catch (error) {
-        //         console.log(error);
-        //         res.status(500).send({ message: error?.message });
-        //     }
-        // })
-
-        // /* Get the testimonials */
-        // app.get('/api/v1/testimonials', async (_req, res) => {
-        //     try {
-        //         const result = await testimonialCollection.find().toArray();
-
-        //         // console.log(result);
-
-        //         res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
-
-        // /* Post a testimonial */
-        // app.post('/api/v1/post-testimonials', async (req, res) => {
-
-        //     try {
-        //         const comment = req.body;
-        //         const result = await testimonialCollection.insertOne(comment);
-
-        //         console.log(result);
-
-        //         res.send(result)
-        //     } catch (error) {
-        //         res.status(500).send({ error: true, message: error.message })
-        //     }
-        // })
 
     } finally {
         // Ensures that the client will close when you finish/error
@@ -579,9 +184,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('SwiftWheels Server is running...')
+    res.send('Ibn Aroub Server is running...')
 })
 
 app.listen(port, () => {
-    console.log(`SwiftWheels app listening on port ${port}`)
+    console.log(`Ibn Aroub app listening on port ${port}`)
 })
